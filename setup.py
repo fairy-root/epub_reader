91ebc165221bb2e2041a59e770ced28b44c32094
fairy-root <badaouiorigin@gmail.com>
2024-07-12 00:09:39 +0100

    updated release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epub-reader',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -18,7 +18,7 @@ setup(
     description='Python CLI Epub reader with page navigation, bookmarks management, search management, save pages and book to text files and reading sessions to save and load your current progress ',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/fairy-root/epub-reader',
+    url='https://github.com/fairy-root/epub_reader',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
